2ecd963ab5dcb97b8c64dcbf689b9c51421277ca
gabmor38 <gabriela.morenor@gmail.com>
2025-12-06 11:33:46 -0500

    patched to new Next version

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-};
+  env: {
+  
+    NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME!,
+  }
+ 
+}
+
 
 export default nextConfig;
